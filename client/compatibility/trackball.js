2e8da8c48e735a58908d5857a506dad0565da0f1
gene <geneshuman@gmail.com>
2013-08-30 18:26:21 -0700

    fixed up mouse

--- client/compatibility/trackball.js
+++ client/compatibility/trackball.js
@@ -1,7 +1,7 @@
 THREE.TrackballControls = function ( object, domElement ) {
 
 	var _this = this;
-	var STATE = { NONE: -1, ROTATE: 0, ZOOM: 1, PAN: 2, TOUCH_ROTATE: 3, TOUCH_ZOOM: 4, TOUCH_PAN: 5 };
+	var STATE = { NONE: -1, ROTATE: 2, ZOOM: 1, PAN: -1, TOUCH_ROTATE: 5, TOUCH_ZOOM: 4, TOUCH_PAN: -1 };
 
 	this.object = object;
 	this.domElement = ( domElement !== undefined ) ? domElement : document;
@@ -18,7 +18,7 @@ THREE.TrackballControls = function ( object, domElement ) {
 
 	this.noRotate = false;
 	this.noZoom = false;
-	this.noPan = false;
+	this.noPan = true;
 	this.noRoll = false;
 
 	this.staticMoving = false;
